86cbb5e50cc2fefd1c6cd6c78ea811ed61776b91
Anthony <anthonyricemath@gmail.com>
2024-10-09 18:33:56 -0400

    rewrote Program.cs file to accomdate https requests

--- PongBlazorApp/Program.cs
+++ PongBlazorApp/Program.cs
@@ -1,4 +1,5 @@
 using PongBlazorApp.Components;
+using System.Net.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,18 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Register HttpClient for server-side Blazor applications
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
