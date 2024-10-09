0ccd1871d34cc2ccf338ba802931ffbca7fc8edc
Anthony <anthonyricemath@gmail.com>
2024-10-09 19:03:46 -0400

    adjusting pong.js location and organizing project

---
+++ PongBlazorApp/wwwroot/js/pong.js
@@ -0,0 +1,27 @@
+﻿window.clearCanvas = function () {
+    var canvas = document.getElementById('pongCanvas');
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+window.drawPaddle = function (x, y, width, height) {
+    var canvas = document.getElementById('pongCanvas');
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = "#0095DD";
+    ctx.fillRect(x, y, width, height);
+};
+
+window.drawBall = function (x, y, radius) {
+    var canvas = document.getElementById('pongCanvas');
+    var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+};
+
+window.setFocus = function (elementId) {
+    document.getElementById(elementId).focus();
+};
+
