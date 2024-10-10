9a30b2c02c7e6d4b18ffb9742d7ee0c8d8445259
Anthony <anthonyricemath@gmail.com>
2024-10-09 20:14:17 -0400

    Not able to call javascript so I used no cSharp and wrote all javascript in Home.razor File. App Works

--- PongBlazorApp/wwwroot/js/pong.js
+++ PongBlazorApp/wwwroot/js/pong.js
@@ -1,4 +1,5 @@
 ﻿window.clearCanvas = function () {
+    console.log('clearCanvas called');  // Log when clearCanvas is called
     var canvas = document.getElementById('pongCanvas');
     if (canvas) {
         var ctx = canvas.getContext('2d');
@@ -13,6 +14,7 @@
 };
 
 window.drawPaddle = function (x, y, width, height) {
+    console.log('drawPaddle called', x, y, width, height);  // Log paddle draw info
     var canvas = document.getElementById('pongCanvas');
     if (canvas) {
         var ctx = canvas.getContext('2d');
@@ -28,6 +30,7 @@ window.drawPaddle = function (x, y, width, height) {
 };
 
 window.drawBall = function (x, y, radius) {
+    console.log('drawBall called', x, y, radius);  // Log ball draw info
     var canvas = document.getElementById('pongCanvas');
     if (canvas) {
         var ctx = canvas.getContext('2d');
@@ -46,6 +49,7 @@ window.drawBall = function (x, y, radius) {
 };
 
 window.setFocus = function (elementId) {
+    console.log('setFocus called for element:', elementId);  // Log focus set info
     var element = document.getElementById(elementId);
     if (element) {
         element.focus();
